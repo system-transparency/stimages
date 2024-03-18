4194538b877ce2de69ee3cfbfa6e13470d585d45
Linus Nordberg <linus.nordberg@glasklarteknik.se>
2024-03-18 20:14:52 +0100

    Create artifact for boot even when job is failing
    Also, clean up qemu process.

--- test/boot.sh
+++ test/boot.sh
@@ -2,6 +2,11 @@
 set -eu
 
 make boot >qemu.log 2>&1 &
+function cleanup() {
+    [[ ! -f qemu.pid ]] || { kill "$(cat qemu.pid)"; rm qemu.pid; }
+}
+trap cleanup EXIT
+
 for delay in 20 2 2 2 2 2 30; do # 1 minute in total
     sleep "${delay}"
     tail qemu.log | grep -q '^example-host login: ' || continue
