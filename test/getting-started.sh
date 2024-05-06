1fa72690d829dea9e1ebdc34d4447faf9690eb5f
Linus Nordberg <linus.nordberg@glasklarteknik.se>
2024-05-06 15:17:37 +0200

    Synchronise test/getting-started.sh with docs/.../build.md
    The only non cosmetical change is the removal of installing
    libsystemd-shared, which if it is necessary should be added to
    build.md too.

--- test/getting-started.sh
+++ test/getting-started.sh
@@ -1,7 +1,7 @@
 #! /bin/bash -x
 set -eu
 
-# Test the ST getting started guide by running the commands in project/docs/content/docs/introduction/build.md.
+# Test the ST build guide by running the commands in project/docs/content/docs/introduction/build.md.
 # NOTE: This file is manually maintained -- when the docs are being changed, someone need to update this file.
 
 # Prerequisites:
@@ -25,14 +25,19 @@ if [ -v container ]; then
 fi
 
 ## project/docs/content/docs/introduction/build.md
+
+### Prepare for booting in QEMU
+[ -z "$(command -v qemu-system-x86_64)" ] && sudo apt install -y qemu-system-x86 ovmf ncat
+
 ### Prepare for building
 [ -z "$(command -v go)" ] && sudo apt install -y golang-go
 [ -z "$(command -v update-ca-certificates)" ] && sudo apt install -y ca-certificates
 [ -z "$(command -v git)" ] && sudo apt install -y git
 [ -z "$(command -v cpio)" ] && sudo apt install -y cpio
-[ -z "$(command -v mmdebstrap)" ] && sudo apt install -y mmdebstrap
 [ -z "$(command -v pigz)" ] && sudo apt install -y pigz
-find /usr/lib* -name libsystemd-shared\*.so -quit || sudo apt install -y libsystemd-shared
+[ -z "$(command -v mmdebstrap)" ] && sudo apt install -y mmdebstrap
+# TODO: any or both of libsystemd-shared and man-db required? If so, update docs/content/docs/introduction/build.md
+#find /usr/lib* -name libsystemd-shared\*.so -quit || sudo apt install -y libsystemd-shared
 #[ ! -e FIXME ] && sudo apt install -y man-db
 
 git clone -b "$STIMAGESVER" https://git.glasklar.is/system-transparency/core/stimages.git
@@ -47,9 +52,6 @@ go install system-transparency.org/stmgr@v0.3.1
 (cd keys && stmgr keygen certificate --isCA)
 (cd keys && stmgr keygen certificate --rootCert rootcert.pem --rootKey rootkey.pem)
 
-### Prepare for booting in QEMU
-[ -z "$(command -v qemu-system-x86_64)" ] && sudo apt install -y qemu-system-x86 ovmf ncat
-
 ## Build your own ST bootloader image
 contrib/stboot/build-stboot http://10.0.2.2:8080/my-os.json keys/rootcert.pem
 
@@ -58,7 +60,7 @@ echo myrootpassword > config/example/pw.root
 ./build-initramfs config/example my-os.cpio.gz
 
 stmgr ospkg create \
-	  --label="My example ST package" \
+	  --label="My example ST system" \
 	  --initramfs=my-os.cpio.gz \
 	  --kernel=my-os.vmlinuz \
 	  --cmdline="console=ttyS0,115200n8 ro rdinit=/lib/systemd/systemd" \
